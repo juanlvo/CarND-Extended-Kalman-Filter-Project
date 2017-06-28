d916836aa16bc10321d97b148b62bbf3de12635d
Juan Vivas <juanlvo@gmail.com>
2017-06-28 14:46:55 +0200

    Feat: Radar messurament
    Implementation of Radar messurament

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -37,7 +37,6 @@ FusionEKF::FusionEKF() {
     * Set the process and measurement noises
   */
 
-
 }
 
 /**
@@ -58,6 +57,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       * Create the covariance matrix.
       * Remember: you'll need to convert radar from polar to cartesian coordinates.
     */
+	ekf_.x_ = VectorXd(4);
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
@@ -65,8 +65,21 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
-      Convert radar from polar to cartesian coordinates and initialize state.
+      Convert radar from polar to Cartesian coordinates and initialize state.
       */
+      float rho = measurement_pack.raw_measurements_[0];
+      float phi = measurement_pack.raw_measurements_[1];
+      float rho_prime = measurement_pack.raw_measurements_[2];
+
+      //Conversion from polar to Cartesian coordinates and initialize it
+
+      float x = rho * cos(phi);
+      float y = rho * sin(phi);
+      float vx = rho_prime * cos(phi);
+      float vy = rho_prime * sin(phi);
+
+      ekf_.x_ << x, y, vx, vy;
+
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
